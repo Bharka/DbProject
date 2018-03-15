100b4f17fbd1ecf87fb35914a1de4c9cecbb0ebe
Bharka <bharka95@gmail.com>
2018-03-15 17:33:16 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/DbProject2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DbProject2.dir/main.cpp.o"
+  "DbProject2.pdb"
+  "DbProject2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DbProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void input(){
+    string name;
+    cin >> name;
+    cout << "this is the name "<<name<<endl;
+}
+
+int main() {
+    cout << "Hello, World!" <<endl;
+    input();
+    return 0;
+}
+
+
